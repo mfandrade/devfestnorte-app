05e8813476b6505a79026a1efb10b8a9027ca44a
Marcelo F Andrade <mfandrade@gmail.com>
2014-10-22 14:27:06 -0300

    Navigation from splash to main

--- src/info/mfandrade/mobile/devfestnorte/SplashActivity.java
+++ src/info/mfandrade/mobile/devfestnorte/SplashActivity.java
@@ -1,7 +1,11 @@
 package info.mfandrade.mobile.devfestnorte;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
 
@@ -9,5 +13,15 @@ public class SplashActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+
+		ImageView splash = (ImageView) findViewById(R.id.splash);
+		splash.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				Intent main = new Intent(SplashActivity.this, MainActivity.class);
+				startActivity(main);
+			}
+		});
 	}
 }
